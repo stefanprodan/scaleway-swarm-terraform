fdb7a6bd3f5e2fe09e9389a4c8dd352febaa1bc0
Stefan Prodan <stefan.prodan@gmail.com>
2017-08-03 20:07:21 +0300

    extract swarm join tokens

---
+++ fetch-tokens.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Processing JSON in shell scripts
+# https://www.terraform.io/docs/providers/external/data_source.html#processing-json-in-shell-scripts
+
+# Exit if any of the intermediate steps fail
+set -e
+
+# Extract "host" argument from the input into HOST shell variable
+eval "$(jq -r '@sh "HOST=\(.host)"')"
+
+# Fetch the manager join token
+MANAGER=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
+root@$HOST docker swarm join-token manager -q)
+
+# Fetch the worker join token
+WORKER=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
+    root@$HOST docker swarm join-token worker -q)
+
+# Produce a JSON object containing the tokens
+jq -n --arg manager "$MANAGER" --arg worker "$WORKER" '{"manager":$manager,"worker":$worker}'
