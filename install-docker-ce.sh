ccfde96fc6a0cb35b48b960915e13f1eda67cfad
Stefan Prodan <stefan.prodan@gmail.com>
2017-08-03 16:16:47 +0300

    swarm manager install

---
+++ install-docker-ce.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# setup Docker repository
+apt-get update -y
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# install Docker CE
+apt-get update -y
+apt-get install -y docker-ce
+
--- main.tf
+++ main.tf
@@ -2,9 +2,11 @@ provider "scaleway" {
   region = "ams1"
 }
 
-data "scaleway_bootscript" "latest" {
+// Racher is required since Scaleway Docker bootstrap is missing IPVS_NFCT and IPVS_RR
+// https://github.com/moby/moby/issues/28168
+data "scaleway_bootscript" "rancher" {
   architecture = "x86_64"
-  name_filter  = "latest"
+  name_filter  = "rancher"
 }
 
 data "scaleway_image" "xenial" {
@@ -12,17 +14,31 @@ data "scaleway_image" "xenial" {
   name         = "Ubuntu Xenial"
 }
 
+resource "scaleway_ip" "swarm_manager_ip" {}
+
 resource "scaleway_server" "swarm_manager" {
   count          = 1
   name           = "swarm_manager-${count.index + 1}"
   image          = "${data.scaleway_image.xenial.id}"
   type           = "VC1S"
-  bootscript     = "${data.scaleway_bootscript.latest.id}"
+  bootscript     = "${data.scaleway_bootscript.rancher.id}"
   security_group = "${scaleway_security_group.swarm_default.id}"
-}
+  public_ip      = "${scaleway_ip.swarm_manager_ip.ip}"
 
-resource "scaleway_ip" "swarm_manager_ip" {
-  server = "${scaleway_server.swarm_manager.id}"
+  connection {
+    type = "ssh"
+    user = "root"
+  }
+
+  provisioner "remote-exec" {
+    script = "install-docker-ce.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "docker swarm init --advertise-addr ${self.private_ip}",
+    ]
+  }
 }
 
 resource "scaleway_security_group" "swarm_default" {
